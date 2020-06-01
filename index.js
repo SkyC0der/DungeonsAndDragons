5d7e868405d7463dcde68e9787c0b439d0e4f9fc
Emmanuel Ezenwigbo <44367769+SkyC0der@users.noreply.github.com>
2020-06-01 22:24:46 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const abilityModifier = c => {
+ //type code here
+}
+
+const dice = () => Math.floor(Math.random() * 6) + 1
+
+//restructure code accordingly
+const Character {
+  //type code here
+}
+
+
+module.exports = {abilityModifier, Character}
