ecf1eaedaa89c3d91422805c114b00c67232f3a3
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-06-02 02:00:42 +0100

    Revert "Implemented function"
    This reverts commit cb805364acf4ea75e3c53f8708e707fc1a6cba22.

--- index.js
+++ index.js
@@ -1,60 +1,14 @@
-//restructure code accordingly
 
-const abilityModifier = (c) => {
-  //type code here
-  if (c > 18 ) throw new Error("Ability scores can be at most 18");
-  if (c < 3 ) throw new Error ("Ability scores must be at least 3");
-  return Math.floor((c - 10) / 2);
-};
-
-class Character {
-  static rollAbility() {
-    const dice = () => Math.floor(Math.random() * 6) + 1;
-    let popins = [];
-    for (let index = 0; index < 4; index++) {
-      let value = dice();
-      popins.push(value);
-    }
-    popins.sort().shift();
-    return popins.reduce((a, b) => a + b);
-  }
-  constructor() {
-    this._strength = Character.rollAbility();
-    this._dexterity = Character.rollAbility();
-    this._constitution = Character.rollAbility();
-    this._intelligence = Character.rollAbility();
-    this._wisdom = Character.rollAbility();
-    this._charisma = Character.rollAbility();
-    this._hitpoints = abilityModifier(this.constitution) + 10;
-  }
-
-  get strength() {
-    return this._strength;
-  }
-
-  get dexterity() {
-    return this._dexterity;
-  }
-
-  get constitution() {
-    return this._constitution;
-  }
-
-  get intelligence() {
-    return this._intelligence;
-  }
-
-  get wisdom() {
-    return this._wisdom;
-  }
-
-  get charisma() {
-    return this._charisma;
-  }
-
-  get hitpoints() {
-    return this._hitpoints;
-  }
+const abilityModifier = c => {
+ //type code here
 }
 
-module.exports = { abilityModifier, Character };
+const dice = () => Math.floor(Math.random() * 6) + 1
+
+//restructure code accordingly
+const Character {
+  //type code here
+}
+
+
+module.exports = {abilityModifier, Character}
